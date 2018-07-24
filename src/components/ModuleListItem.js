b66e6442fd3c9c8e3f92843386a3df7cb9a5e111
Akriti <akriti.bhat28@gmail.com>
2018-07-23 20:14:27 -0400

    Final Changes

--- src/components/ModuleListItem.js
+++ src/components/ModuleListItem.js
@@ -5,12 +5,21 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 export default class ModuleListItem
   extends React.Component {
 
+    highlight(event){
+       var p= event.target.parentNode ;
+       p.style.backgroundColor="cyan";
+       var list= document.getElementsByClassName('highlightclass');
+       for(var i=0;i<list.length;i++){
+           list[i].style.backgroundColor="white";
+       }
+        p.style.backgroundColor="cyan";
+    }
     render() {
     return (
 
-      <li className="list-group-item">
+      <li className="list-group-item highlightclass">
 
-          <Link to={`/course/${this.props.courseId}/module/${this.props.module.id}/lesson`}>
+          <Link  onClick={this.highlight} to={`/course/${this.props.courseId}/module/${this.props.module.id}/lesson`}>
               {this.props.module.title}
               <button className="btn float-right"><i className="fa fa-pencil"></i></button>
           </Link>
